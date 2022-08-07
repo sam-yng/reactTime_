6de2f475a9fb63251a50981dc808a88f448e23e1
samy <young142001@hotmail.com>
2022-08-08 01:32:20 +1000

    tailwind implem

--- trial-react-app/src/App.js
+++ trial-react-app/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './index.css';
+
+import firstImage from './assets/images/outline-b.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="bg-green-600">
+      <div>
+        <img src={firstImage} />
+        <div>
+          <h1>fuck this</h1>
+        </div>
+
+
+      </div >
     </div>
-  );
+  )
 }
 
 export default App;
